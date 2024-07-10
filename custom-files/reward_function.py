3a8890c31670bb750df92ee054b22efba366ab93
Nauman Shaikh <nauman.m.shaikh@gmail.com>
2024-07-10 18:36:00 +0100

    Model 6

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,52 +1,22 @@
-import math
-
 def reward_function(params):
-
-    # Read input variables
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
-
-    track_width = params['track_width']
-    distance_from_center = params['distance_from_center']
-    all_wheels_on_track = params['all_wheels_on_track']
-    speed = params['speed']
+    # Read input variable
+    steps = params['steps']
     progress = params['progress']
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
 
-    # Give a very low reward by default
-    reward = 1e-6
+    # Total num of steps we want the car to finish the lap, it will vary depends on the track length
+    TOTAL_NUM_STEPS = 300
 
-    # Make the reward equal to progress if no wheels go off the track and
-    # the agent is somewhere in between the track borders
-    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
-        reward = progress / 100
+    # Initialize the reward with typical value
+    reward = 1.0
 
-    # Calculate the direction of the center line based on the closest waypoints
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
+    # Give additional reward if the car pass every 100 steps faster than expected
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        reward += 10.0
 
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
+    if not (all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05):
+        reward = 1e-6
 
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-
-    # Function to adjust the reward based on the speed and heading of the car
-    DIRECTION_THRESHOLD = 30.0
-    DIRECTION_OPTIMUM = 0.0
-    MAX_SPEED = 3.0
-    MIN_SPEED = 0.5
-
-    if direction_diff > DIRECTION_THRESHOLD:
-        direction_diff = DIRECTION_THRESHOLD
-
-    normalised_speed = (speed - MIN_SPEED) / (MAX_SPEED - MIN_SPEED)
-    normalised_heading = (direction_diff - DIRECTION_OPTIMUM) / (DIRECTION_THRESHOLD - DIRECTION_OPTIMUM)
-    
-    z = (normalised_speed - normalised_heading + 1) / 2
-
-    return float(reward) + z
+    return float(reward)
