51639d763e6a17e4f59dfa38a14cbd21ba063c02
Nauman Shaikh <nauman.m.shaikh@gmail.com>
2024-07-12 12:26:40 +0100

    Update reward_function to reduce NUMOFSTEPS

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -5,9 +5,10 @@ def reward_function(params):
     all_wheels_on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
+    is_reversed = params['is_reversed']
 
     # Total num of steps we want the car to finish the lap, it will vary depends on the track length
-    TOTAL_NUM_STEPS = 300
+    TOTAL_NUM_STEPS = 200
 
     # Initialize the reward with typical value
     reward = 1.0
@@ -16,7 +17,7 @@ def reward_function(params):
     if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
         reward += 10.0
 
-    if not (all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05):
+    if not (all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05 and not is_reversed):
         reward = 1e-6
 
     return float(reward)
