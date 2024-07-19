51b3034ab433066758d13de3cf76dd5953f0512e
Nauman Shaikh <nauman.m.shaikh@gmail.com>
2024-07-19 16:29:07 +0100

    Model 1.0.0 for empire track

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,6 +1,12 @@
 def reward_function(params):
+    #############################################################################
+    '''
+    Based off of example at
+    https://docs.aws.amazon.com/deepracer/latest/developerguide/deepracer-reward-function-input.html#reward-function-input-steps
+    '''
     
     # Read input variable
+    speed = params['speed']
     steps = params['steps']
     progress = params['progress']
     all_wheels_on_track = params['all_wheels_on_track']
@@ -12,13 +18,13 @@ def reward_function(params):
     TOTAL_NUM_STEPS = 200
 
     # Initialize the reward with typical value
-    reward = -1
+    reward = 0.1 * ((speed - 1.0) / 3)
+
+    # Give additional reward if the car pass every 25 steps faster than expected
+    if (steps % 25) == 0:
+        reward += (progress / steps)
 
     if not (all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05 and not is_reversed):
-        return float(-100)
-    
-    if progress % 10 == 0:
-        reward = progress * 5
+        reward = 1e-6
 
     return float(reward)
-    
